691f346c240a5fffc2f681e4e930b5ecf4e473d7
Elías <solerurban.elias@gmail.com>
2021-12-14 17:09:34 +0100

    añadida tabla usuario

---
+++ dump/usuario.sql
@@ -0,0 +1,9 @@
+use cholloSevero;
+create table usuario(
+    id int primary key auto_increment,
+    nombre varchar(256) not null,
+    login varchar(256) not null,
+    password varchar(256) not null,
+    rol enum("usuario", "administrador") not null
+)
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci;

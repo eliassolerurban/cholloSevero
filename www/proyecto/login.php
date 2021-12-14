887d1e1b38b22cdc2d97de553f428bba31ae21e6
Elías <solerurban.elias@gmail.com>
2021-12-14 19:49:31 +0100

    funcion confirmPOST hecha

---
+++ www/proyecto/login.php
@@ -0,0 +1,10 @@
+<?php
+
+include "utils/functions.php";
+
+$login = confirmPOST("login", "formLogin.php");
+$password = confirmPOST("password", "formLogin.php");
+
+
+
+
--- www/proyecto/utils/functions.php
+++ www/proyecto/utils/functions.php
@@ -4,9 +4,18 @@ include "connection.php";
 include "queries.php";
 include "exceptions.php";
 
-function conexion() {
+function conexion(){
     $conexion = new PDO(DSN, USUARIO, PASSWORD);
-    $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $conexion;
 }
 
+function confirmPOST($campo, $vuelta){
+    if (isset($_POST[$campo])) {
+        return $_POST[$campo];
+    } else {
+        $error = "debes de indicar el $campo";
+        include $vuelta;
+        die();
+    }
+}
